8bf17100ab0f376e4bf92cd41d79997bbfb80b54
ange07001 <ange07001@utb.vaxjo.se>
2024-09-25 13:05:23 +0200

    Completed Triangle-program

--- triangel.py
+++ triangel.py
@@ -1,4 +1,22 @@
-try:
-    while True:
-        n = int(input("Skriv ett positivt udda heltal"))
-except ValueError:
+while True:
+    try:
+        n = int(input("Skriv ett positivt udda heltal: "))
+        if n%2 == 1 and n > 0:
+            break
+        else:
+            print("Inte ett positivt udda tal!")
+    except ValueError:
+        print("Inte ett heltal!")
+
+def RightTriangle(n):
+    print("\nDetta är en rätvinklig triangel:\n")
+    for i in range(n):
+        print(" "*i + "*"*(n-i))
+
+def IsoscelesTriangle(n):
+    print("\nDetta är Isoscelers triangel:\n")
+    for i in range(0,n,2):
+        print(" "*(n//2-i//2) + "*"*(i+1))
+
+RightTriangle(n)
+IsoscelesTriangle(n)
